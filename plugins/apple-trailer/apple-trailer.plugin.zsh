1fb94f2a19faaf708b1611030a6ec52eb9fc3649
Svein Harald Soleim <svein.harald.soleim@imr.no>
2013-05-02 10:52:19 +0200

    added apple-trailer plugin

---
+++ plugins/apple-trailer/apple-trailer.plugin.zsh
@@ -0,0 +1,7 @@
+#Download trailers from http://trailers.apple.com
+
+# wget options are:
+#  -c - Continue getting a partially-downloaded file.
+#  -U - Identify as agent-string to the HTTP server.
+
+alias apple='wget -c -U "QuickTime is not necessary to watch a trailer!"'
